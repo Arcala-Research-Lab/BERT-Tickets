3666d11b3f8ee2ba411b94192e7a2cb8b454e360
maxwell-sterner <maxsterner11@gmail.com>
2024-01-27 08:48:51 -0800

    lt glue

--- LT_glue.py
+++ LT_glue.py
@@ -842,7 +842,7 @@ def main():
 
     #         model = model_class.from_pretrained(checkpoint)
     #         model.to(args.device)
-            result = evaluate(args, model, tokenizer, prefix=prefix)
+            # result = evaluate(args, model, tokenizer, prefix=prefix)
     #         result = dict((k + "_{}".format(global_step), v) for k, v in result.items())
     #         results.update(result)
     #     for key in results.keys():
